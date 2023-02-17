9ae46c8b4e9847ec31ad253e8bb3a679aea116c5
EDonnellan <elaine.donny@gmail.com>
2023-02-17 02:35:27 +0000

    breedFetcher func, with index file to run it and test folder and file to test with mocha.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,27 +1,23 @@
 const request = require("request");
-const args = process.argv.slice(2);
-console.log("args ", args);
 
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request.get(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
 
-const catBreedToSearch = args[0].toLowerCase();
-console.log("catBreed ", catBreedToSearch);
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${catBreedToSearch}`;
-console.log("url ", url);
+    const [data] = JSON.parse(body);
+    
+    if (!data) {
+      console.log("hello");
+      callback(null, "Sorry breed not found. Please try again!");
+      return;
+    }
+    
+    callback(null, data.description);
+  });
+};
 
-
-
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  
-  const data = JSON.parse(body);
-  
-  if (data.length === 0) {
-    console.log("Sorry breed not found. Please try again!");
-    return;
-  }
-  
-  console.log(data["0"].description);
-});
+module.exports = {fetchBreedDescription};
